566438d20dac0272fb0106b6fad90ce8bf567b51
hamiltonbrba <Bryanahamiltonbrown@gmail.com>
2024-09-01 13:05:38 -0400

    began app.py database setup

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,27 +1,33 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
 
+# create an engine to connect to the SQLite database
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
 
 
 
